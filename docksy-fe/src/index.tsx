ba5fc8fa31e74ef1bbd5d0c775b010c3e56862d3
Donadai <79592184+Donadai@users.noreply.github.com>
2023-10-30 15:41:29 +0200

    Initial Auth0 integration

--- docksy-fe/src/index.tsx
+++ docksy-fe/src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-2z4tzep84k14rx4f.us.auth0.com"
+      clientId="9vkeNLnr66rztHlc5gJvrM7JHT2GOD2j"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
