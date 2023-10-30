5183d8279828dab6fea3aa01e32584245168bb68
Donadai <79592184+Donadai@users.noreply.github.com>
2023-10-30 17:06:24 +0200

    Add background

--- docksy-fe/src/App.tsx
+++ docksy-fe/src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
-    <div>
-      <LoginButton />
-      <LogoutButton />
-    </div>
+    <>
+    </>
   );
 }
 
