fac52a1591df91f412fd082d3c5e0f67b38defcd
Donadai <79592184+Donadai@users.noreply.github.com>
2023-11-01 13:53:43 +0200

    Modify Header component

--- docksy-fe/src/App.tsx
+++ docksy-fe/src/App.tsx
@@ -4,12 +4,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Search from './components/search/Search';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Header />
+        <Header isAuthenticated={isAuthenticated} />
         <Routes>
           <Route path="/" element={<Search />} />
           <Route path="/search" element={<Search />} />
--- docksy-fe/src/components/header/Header.tsx
+++ docksy-fe/src/components/header/Header.tsx
@@ -1,17 +1,42 @@
+import './Header.scss';
+
+import { useAuth0 } from '@auth0/auth0-react';
 import { Tabs, Tab, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Header() {
-  return (
-    <Box>
-      <Tabs value={false}>
-        <Tab label="Search" component={Link} to="/search" />
-        <Tab label="Explore" component={Link} to="/explore" />
-        <Tab label="Saved" component={Link} to="/saved" />
-        <Tab label="Bookings" component={Link} to="/bookings" />
-        <Tab label="Profile" component={Link} to="/profile" />
-      </Tabs>
-    </Box>
-  );
+interface HeaderProps {
+  isAuthenticated: boolean;
 }
+
+const Header: React.FC<HeaderProps> = ({ isAuthenticated }) => {
+  const { loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <div className="header-container">
+      <div className="logo">
+        <img src="/images/docksy/docksy-logo.png" alt="Docksy Logo" className="logo" />
+      </div>
+      <div className="search-explore-tabs">
+        <Tabs value={false}>
+          <Tab label="Search" component={Link} to="/search" />
+          <Tab label="Explore" component={Link} to="/explore" />
+        </Tabs>
+      </div>
+      <div className="other-tabs">
+        <Tabs value={false}>
+          {isAuthenticated ? (
+            <>
+              <Tab label="Saved" component={Link} to="/saved" />
+              <Tab label="Bookings" component={Link} to="/bookings" />
+              <Tab label="Profile" component={Link} to="/profile" />
+              <Tab label="Logout" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} />
+            </>
+          ) : (
+            <Tab label="Login" onClick={() => loginWithRedirect()} />
+          )}
+        </Tabs>
+      </div>
+    </div>
+  );
+};
 export default Header;
